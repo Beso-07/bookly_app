14fcbd02e0f56656854465c106b403fb27770f21
Beso-07 <mahmoudbassem34@gmail.com>
2025-09-17 19:04:31 +0300

    similar books list view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/features/home/presentation/views/widgets/book_actions
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_cover.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -20,11 +21,10 @@ class BookDetailsViewBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: width * .18),
             child: CustomBookCover(),
           ),
-          const SizedBox(height: 40),
+          const SizedBox(height: 30),
           Text(
             'The Jungle Book',
             style: Styles.textStyle30.copyWith(fontWeight: FontWeight.w600),
-            // textAlign: TextAlign.center,
           ),
           const SizedBox(height: 6),
           Opacity(
@@ -39,10 +39,23 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           const SizedBox(height: 15),
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          const SizedBox(height: 33),
-          BookActions(),
+          const SizedBox(height: 15),
+          const BookActions(),
+          const SizedBox(height: 30),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like ',
+              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 10),
+
+          SimilarBooksListView(),
         ],
       ),
     );
   }
 }
+
+

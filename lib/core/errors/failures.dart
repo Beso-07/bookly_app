58eab293c819e465e32faca4ce4ba8c9e2600489
Beso-07 <mahmoudbassem34@gmail.com>
2025-09-18 18:12:27 +0300

    create server failure

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,55 @@
-abstract class Failures {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failures {}
+abstract class Failures {
+  final String errMessage;
+
+  const Failures(this.errMessage);
+}
+
+class ServerFailure extends Failures {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout With ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout With ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout With ApiServer');
+
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Invalid SSL certificate from API server');
+
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          dioException.response!.statusCode!,
+          dioException.response!.data,
+        );
+
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was cancled');
+
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection timeout With ApiServer');
+
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No internet Connection');
+        }
+        return ServerFailure('Unexpected error, please try again!');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your request not found, please try again later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error, please try agin');
+    } else {
+      return ServerFailure('Opps there was an error, please try again');
+    }
+  }
+}
--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:dio/dio.dart';
 
 class ApiServices {

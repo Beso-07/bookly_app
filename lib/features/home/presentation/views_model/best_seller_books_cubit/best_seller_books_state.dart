76d8ea41670287de23a85726f135da93d5dac36b
Beso-07 <mahmoudbassem34@gmail.com>
2025-09-18 18:33:38 +0300

    best seller books cubit

---
+++ lib/features/home/presentation/views_model/best_seller_books_cubit/best_seller_books_state.dart
@@ -0,0 +1,23 @@
+part of 'best_seller_books_cubit.dart';
+
+sealed class BestSellerBooksState extends Equatable {
+  const BestSellerBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class BestSellerBooksInitial extends BestSellerBooksState {}
+
+final class BestSellerBooksLoading extends BestSellerBooksState {}
+
+final class BestSellerBooksFailure extends BestSellerBooksState {
+  final String errMesage;
+
+  const BestSellerBooksFailure(this.errMesage);
+}
+
+final class BestSellerBooksSuccess extends BestSellerBooksState {
+  final List<BookModel> books;
+  const BestSellerBooksSuccess(this.books);
+}
--- lib/features/home/presentation/views_model/cover_books_cubit/cover_books_cubit.dart
+++ lib/features/home/presentation/views_model/cover_books_cubit/cover_books_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'cover_books_state.dart';
 
 class CoverBooksCubit extends Cubit<CoverBooksState> {
-  CoverBooksCubit() : super(CoverBooksInitial());
+  CoverBooksCubit(this.homeRepo) : super(CoverBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchCoverBooks() async {
+    emit(CoverBooksLoading());
+    var result = await homeRepo.fetchCoverBooks();
+
+    result.fold(
+      (failure) => emit(CoverBooksFailure(failure.errMessage)),
+      (books) => emit(CoverBooksSuccess(books)),
+    );
+  }
 }
